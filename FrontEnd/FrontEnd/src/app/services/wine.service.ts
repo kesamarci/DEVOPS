9d3f1db254b0edc2e3d9e29cfee07194bf246f6d
Keserű Márton <gergelymarton00@gmail.com>
2025-11-21 16:30:10 +0100

    environments uppdate

--- FrontEnd/FrontEnd/src/app/services/wine.service.ts
+++ FrontEnd/FrontEnd/src/app/services/wine.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Wine } from '../models/wine';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
--- FrontEnd/FrontEnd/src/environments/environment.ts
+++ FrontEnd/FrontEnd/src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    apiUrl: "http://localhost:5210"
+    backendUrl: "http://localhost:5210"
 };
